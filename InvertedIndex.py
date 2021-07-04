659a2e5aadac7113a917dcc075e6cf4158533c31
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2021-07-04 02:15:39 -0500

    load data from disco if inv_idx is empty - InvertedIndex

--- InvertedIndex.py
+++ InvertedIndex.py
@@ -9,6 +9,7 @@ __email__ = "gchacaltanab@outlook.com"
 __version__ = "1.0.1"
 
 import sys
+import ast
 from glob import glob
 from functools import reduce
 
@@ -17,28 +18,51 @@ class InvertedIndex(object):
     def __init__(self, path_directory_documents):
         self.texts = {}
         self.words = set()
-        self.dict_inv_idx = {}
+        self.inverted_index = {}
         self.encoding = "utf8"
         self.path_directory = path_directory_documents
+        self.name_file_inv_idx = "data_inv_idx.txt"
+        self.run()
+
+    def run(self):
         self.load_documents()
-        self.create_dict_inv_index()
+        self.create()
+        self.save()
 
     # Carga el contenido de los ficheros *.txt
     def load_documents(self):
+        counter_files = 0
         for file in glob(self.path_directory):
+            counter_files = counter_files + 1
             with open(file, encoding=self.encoding) as f:
                 content = f.read().split()
                 self.words |= set(content)
                 self.texts[file.split('\\')[-1]] = content
+        print("\nSe procesaron {} documentos. Ruta: {}".format(counter_files, self.path_directory))
 
     # Crea el índice invertido
-    def create_dict_inv_index(self):
-        self.dict_inv_idx = {word: set(txt for txt, words in self.texts.items()
-                          if word in words) for word in self.words}
+    def create(self):
+        self.inverted_index = {word: set(txt for txt, words in self.texts.items()
+                                         if word in words) for word in self.words}
+        print("Se creo el indice invertido")
+
+    def save(self):
+        f = open(self.name_file_inv_idx, "w")
+        f.write(str(self.inverted_index))
+        f.close()
+        print("Se guardo la informacion del indice invertido en disco")
+
+    def read_data(self):
+        if not self.inverted_index:
+            f = open (self.name_file_inv_idx,'r')
+            content = f.read()
+            f.close()
+            self.inverted_index = ast.literal_eval(content)
 
     # Búsqueda de palabras en el índice invertido
     def search_words(self, words):
+        self.read_data()
         try:
-            return reduce(set.intersection, (self.dict_inv_idx[word] for word in words), set(self.texts.keys()))
+            return reduce(set.intersection, (self.inverted_index[word] for word in words), set(self.texts.keys()))
         except (Exception, KeyError, IndexError) as err:
             return ""
