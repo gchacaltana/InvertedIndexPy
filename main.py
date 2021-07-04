fb9368e6f844227fdbfc8036e5837d706c755e6c
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2021-07-04 11:11:45 -0500

    add filter to get specific files

--- main.py
+++ main.py
@@ -12,21 +12,26 @@ __author__ = "Gonzalo Chacaltana"
 __email__ = "gchacaltanab@outlook.com"
 
 if __name__ == "__main__":
-    path_directory_docs = str(pathlib.Path(
-        __file__).parent.resolve()) + "/documents/*.txt"
-    ii = InvertedIndex(path_directory_docs)
+    # definimos directorio donde se encuentran los documentos (comentarios de tweet).
+    path_directory_docs = str(pathlib.Path(__file__).parent.resolve()) + "/documents/"
+    # definimos el filtro a considerar en la extracción de archivos.
+    filter_files = "*.txt"
 
-    # Búsqueda de palabras
+    # instanciamos la clase InvertedIndex en la variable "ii"
+    ii = InvertedIndex(path_directory_docs, filter_files)
+
+    # Realizamos la búsqueda de documentos relacionados a la palabra "solicitud"
     search_words = ["solicitud"]
-    print('\nBusqueda de: ' + repr(search_words))
+    print('\nBúsqueda de: ' + repr(search_words))
     pp(sorted(ii.search_words(search_words)))
 
+    # Realizamos la búsqueda de documentos relacionados a la palabra "banco"
     search_words = ["banco"]
-    print('\nBusqueda de: ' + repr(search_words))
+    print('\nBúsqueda de: ' + repr(search_words))
     pp(sorted(ii.search_words(search_words)))
 
-    # Búsqueda de documentos por intersección de palabras
-    print('\nInterseccion de palabras ')
+    # Realizamos la búsqueda de documentos por intersección de palabras
+    print('\nIntersección de palabras ')
     search_words = ["solicitud", "banco"]
-    print('Busqueda de: ' + repr(search_words))
+    print('Búsqueda de: ' + repr(search_words))
     pp(sorted(ii.search_words(search_words)))
